a56eaf10ed1a05f168085b68446ccdb91f7d9701
alex1001A <alexiordanov1998@gmail.com>
2024-02-08 16:59:22 +0200

    data types all hometasks

--- lesson-2_Data types/homework/Norm/main.js
+++
@@ -1,5 +0,0 @@
-let inp1One = document.querySelector('.i-1-1').value;
-let inp1Two = document.querySelector('.i-1-2').value;
-let btn2 = document.querySelector('.b-1');
-let out1 = document.querySelector('.out-1');
-
