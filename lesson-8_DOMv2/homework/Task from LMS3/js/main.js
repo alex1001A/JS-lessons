6a62b6157952bb3a284700477586df3126dd2c07
alex1001A <alexiordanov1998@gmail.com>
2024-02-23 23:43:04 +0200

    Traffic Light 2

--- lesson-8_DOMv2/homework/Task from LMS3/js/main.js
+++ lesson-8_DOMv2/homework/Task from LMS3/js/main.js
@@ -4,6 +4,7 @@ let btnSwitch = document.querySelector('.btn')
 btnSwitch.addEventListener('click', () => {
     
     trafficLight.forEach(item => {
+        console.log(item.index);
         item.classList.toggle('red')
     })
 
