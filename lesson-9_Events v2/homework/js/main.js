79dc83dbee01204a389fc1ef6d182f27c2b781b1
alex1001A <alexiordanov1998@gmail.com>
2024-02-25 23:59:47 +0200

    end HW lesson 9 Evens V2

--- lesson-9_Events v2/homework/js/main.js
+++ lesson-9_Events v2/homework/js/main.js
@@ -4,30 +4,21 @@ let btn = document.querySelector('.btn')
 let out = document.querySelector('.out')
 let count = document.querySelector('.count')
 
-
 let items = []
-
-//  Приорітети
-// Low = green
-// middle = yellow
-// high = red
-
-// let testObj = {
-//     name: "Sasha",
-//     peotity: 'high'
-// }
 
 btn.addEventListener('click', () => {
 
-    items.push({
+    const newItem = {
         name: inp.value,
-        peotity: sel.value,
-    })
+        priority: sel.value,
+    };
+
+    items.push(newItem);
 
     out.innerHTML = ''
 
-    items.map(item => {
-        out.append(createTask(item))
+    items.map((item, index) => {
+        out.append(createTask(item, index))
     })
 
     console.log(items);
@@ -37,7 +28,8 @@ btn.addEventListener('click', () => {
     count.innerHTML = +count.innerHTML + 1
 })
 
-function createTask(item) {
+
+function createTask(item, index) {
 
     let li = document.createElement('li')
     li.classList.add('li')
@@ -47,11 +39,24 @@ function createTask(item) {
 
     let spanName = document.createElement('span')
     spanName.classList.add('span')
-    spanName.innerHTML = `${item.name} `
+    spanName.innerHTML = `${item.name}`
+    spanName.setAttribute('contenteditable', 'false')
+
+    if (item.priority == 'low') {
+        spanName.classList.add('low')
+    } else if (item.priority == 'middle') {
+        spanName.classList.add('middle')
+    } else if (item.priority == 'high') {
+        spanName.classList.add('high')
+    }
+
+    let editBtn = document.createElement('button')
+    editBtn.classList.add('btn-edit')
+    editBtn.innerHTML = '✏️'
 
     let btnDelete = document.createElement('button')
     btnDelete.classList.add('btn-remove')
-    btnDelete.innerHTML = 'X'
+    btnDelete.innerHTML = '❌'
 
     inpCheck.addEventListener('change', () => {
             if (inpCheck.checked) {
@@ -61,19 +66,58 @@ function createTask(item) {
             }
     })
 
+    function removeTask(item, index) {
+        if (inpCheck.checked == false && items.splice(index, 1)) {
+            count.innerHTML = +count.innerHTML - 1
+        } else if (inpCheck.checked == true && items.splice(index, 1)) {
+            count.innerHTML = count.innerHTML
+        }
+        // Удаляем элемент из массива по индексу
+        // items.splice(index, 1);
+        // Перерисовываем список задач
+        out.innerHTML = '';
+        items.map((item, index) => {
+            out.append(createTask(item, index));
+        });
+        // есть бага, если задача Чекнута, и мы ее удаляем, то слетают все 
+        // чекбоксы, которые так же были отмечены
+    }
+    
+    btnDelete.addEventListener('click', () => {
+        removeTask(item, index)
+    })
+    
+    let click = 1
+    
+    function editTask() {
+        
+
+        if (click == 1) {
+            spanName.contentEditable = 'true'
+            click = 2
+        } else if(click == 2) {
+            spanName.contentEditable = 'false'
+            click = 1
+        }
+    }
+    
+    editBtn.addEventListener('click', () => {
+        editTask()
+    })
+    
     inpCheck.addEventListener('change', () => {
         if (inpCheck.checked) {
             count.innerHTML = +count.innerHTML - 1
         } else {
             count.innerHTML = +count.innerHTML + 1
         }
-
     })
 
-    li.append(inpCheck, spanName, btnDelete)
+    li.append(inpCheck, spanName, editBtn, btnDelete)
 
     return li
 }
+
 
 
 
