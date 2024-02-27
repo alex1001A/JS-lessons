e807cefabcb97bab3d2f9d161bc333ae94dcfb09
alex1001A <alexiordanov1998@gmail.com>
2024-02-27 18:58:18 +0200

    little training arrays

--- lesson-8_DOMv2/homework/Task from LMS3/js/main.js
+++ lesson-8_DOMv2/homework/Task from LMS3/js/main.js
@@ -1,11 +1,25 @@
-let trafficLight = document.querySelectorAll('.circles')
-let btnSwitch = document.querySelector('.btn')
+let circles = document.querySelectorAll('.circles');
+let btnSwitch = document.querySelector('.btn');
+
+let click = 0;
 
 btnSwitch.addEventListener('click', () => {
-    
-    trafficLight.forEach(item => {
-        console.log(item.index);
-        item.classList.toggle('red')
-    })
+    click = (click + 1) % 3; // Используем остаток от деления для циклического изменения значения от 0 до 2
 
-})
+    circles.forEach((circle, index) => {
+        circle.classList.remove('red', 'yellow', 'green');
+        if (index === click) {
+            switch (click) {
+                case 0:
+                    circle.classList.add('red');
+                    break;
+                case 1:
+                    circle.classList.add('yellow');
+                    break;
+                case 2:
+                    circle.classList.add('green');
+                    break;
+            }
+        }
+    });
+});
